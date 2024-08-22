ecc5a834eea69175f87d8049c3427526919992b5
anarb0 <anarb359@gmail.com>
2024-08-22 19:11:24 -0400

    update

--- array/index.js
+++ array/index.js
@@ -40,12 +40,55 @@
 // console.log(array[0][1].name);
 
 
+// Reverse Array
+
+// const array = [1, 2, 3, 4, 5];
+// const newArray = [];
+
+// for (let i = array.length - 1; i >= 0; i--) {
+//     newArray[newArray.length] = array[i];
+// }
+// console.log(newArray)
 
 
-const array = [1, 2, 3, 4, 5];
-const newArray = [];
 
-for (let i = array.length - 1; i >= 0; i--) {
-    newArray[newArray.length] = array[i];
-}
-console.log(newArray)
+
+// Array Methods
+
+
+// Reverse
+// const array = [1, 2, 3, 4, 5];
+// array.reverse();
+// console.log(array);
+
+
+// Sort
+// const array = [2, 3, 1, 0, 4];
+// array.sort();
+// console.log(array);
+
+
+// toString
+// const fruits = ["Banana", "Apple", "Orange", "Mango"];
+// console.log(fruits.toString());
+
+
+// // Includes
+// const array = [100, 200];
+// console.log(array.includes(200));
+
+
+// Push
+// const array = [1, 2, 3];
+// array.push(4);
+// console.log(array);
+
+
+// Pop
+// const array = [1, 2, 3];
+// array.pop();
+// console.log(array);
+
+
+
+

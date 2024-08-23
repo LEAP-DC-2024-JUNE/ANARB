0109556cbc3e74fd185041e0abc4fc64b59aaa48
anarb0 <anarb359@gmail.com>
2024-08-22 21:42:38 -0400

    Grid responsive

--- array/index.js
+++ array/index.js
@@ -104,18 +104,47 @@
 
 
 
-let n = prompt();
-let result = 1;
-let pow = 0;
+// let n = prompt();
+// let result = 1;
+// let pow = 0;
 
-while (result < 1000) {
-    if (n == 1 || n >= 1000) {
-        console.log("Боломжгүй")
-        break;
-    } 
-    else {
-        pow++;
-        result = n ** pow;
-    }
-}
-console.log(n,"-н", pow, "зэрэг 1000-с их байна")
+// while (result < 1000) {
+//     if (n == 1 || n >= 1000) {
+//         console.log("Боломжгүй")
+//         break;
+//     } 
+//     else {
+//         pow++;
+//         result = n ** pow;
+//     }
+// }
+// console.log(n,"-н", pow, "зэрэг 1000-с их байна")
+
+
+// Includes Method
+
+// const array = [100, 200, 300, 400, 500];
+// let inc = prompt();
+
+// for (let i = 0; i <= array.length; i++) {
+//     if (array[i] == inc) {
+//         console.log("Array includes", inc)
+//         break;
+//     }
+//     else {
+//         console.log("Array does not includes", inc)
+//         break;
+//     }
+// }
+
+
+
+// Sort Method
+// const array = [2, 3, 1, 0, 4];
+// const newArray = [];
+// for (let i = array.length - 1; i >= 0; i--) {
+//     if (array[i] > array [i-1]) {
+//         newArray[array.length-1] = array[i]
+//     }
+// }
+// console.log(newArray)

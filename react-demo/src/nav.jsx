2ee8bc168ae8a66dff5c1d4e9fd5bf88d4ff2ed8
anarb0 <anarb359@gmail.com>
2024-08-29 09:48:57 -0400

    nav bar jump to other pages

--- react-demo/src/nav.jsx
+++ react-demo/src/nav.jsx
@@ -1,15 +1,29 @@
 import "./nav.css";
 import "./navbar.css";
 import "./btn.css";
+import "./text.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div className="Nav">
       <div className="Navbar">
         <span>Brand</span>
-        <span>Home</span>
-        <span>About</span>
-        <span>Products</span>
+        <span>
+          <Link className="text" to={"/home"}>
+            Home
+          </Link>
+        </span>
+        <span>
+          <Link className="text" to={"/about"}>
+            About
+          </Link>
+        </span>
+        <span>
+          <Link className="text" to={"/products"}>
+            Products
+          </Link>
+        </span>
       </div>
       <button className="btn">Search</button>
     </div>

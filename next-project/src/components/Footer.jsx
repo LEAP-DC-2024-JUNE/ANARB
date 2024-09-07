2736193ce65bed852ff89dbf11e5b0cb1ed58193
anarb0 <anarb359@gmail.com>
2024-09-06 23:47:35 -0400

    fix

--- next-project/src/components/Footer.jsx
+++ next-project/src/components/Footer.jsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import Copyright from "../../public/CopyrightIcon.png";
-
 function Footer() {
   return (
     <div className="w-full px-20 py-6 bg-gray-50">
       <div className="px-8 flex justify-center">
-        <Image src={Copyright} />
         <p className="font-normal text-lg text-gray-600 text-center">
-          2024 | Greetings with ❤️️ from Ulaanbaatar.
+          © 2024 | Greetings with ❤️️ from Ulaanbaatar.
         </p>
       </div>
     </div>

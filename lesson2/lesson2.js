cb51eb1b1e6ada54d24763e89ed6e3545b7f57bb
anarb0 <anarb359@gmail.com>
2024-10-29 21:43:10 -0400

    web server API

--- lesson2/lesson2.js
+++ lesson2/lesson2.js
@@ -1,7 +1,27 @@
 const http = require("http");
+const fs = require("fs");
+
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!!!");
+  let path = req.url;
+
+  if (path === "" || path === "/" || path === "/home") {
+    res.end("Hello, this is Home Page");
+  } else if (path === "/products") {
+    res.end("Hello, this is Products Page");
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
